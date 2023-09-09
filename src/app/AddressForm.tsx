062555a9fa4b4db4cedffe1e267885f20d81b137
KBeKind <klee@LAPTOP-5P7KDISB>
2023-09-08 19:11:06 -0500

    updated code with comments to explain what is going on.  adjusted styling some via tailwind

--- src/app/AddressForm.tsx
+++ src/app/AddressForm.tsx
@@ -2,19 +2,41 @@
 import React, { ChangeEvent, FormEvent, useState } from 'react';
 
 
+// The props object is a standard component in React.
+// It is used to pass data from a parent component to a child component.
+// The props object can contain any type of data, such as strings, numbers, objects, and functions.
+// In this code the props object is passed to the AddressForm component from the parent component.
+// The handler property of the props object is a function that is called when the form is submitted.
+// The address parameter of the handler function is the value of the address input field.
 function AddressForm(props: { handler: (address: string) => void }) {
 
-  const [values, setValues] = useState({
+  // useState is a React Hook that lets you add a state variable to your component.
+  // const [state, setState] = useState(initialState);
+  // setting the values state variable as an object with a property of address initialized as a blank string
+    const [values, setValues] = useState({
     address: '',
   });
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    // the default submission event is prevented
     e.preventDefault();
+    // the handler function is called here with the values.address
     props.handler(values.address)
   };
 
-  const handleAddressInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+// handleAddressInputChange takes one argument, event, which is an event object
+// that is emitted whenever the value of the address input changes.
+   const handleAddressInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+
+    // event.persist(), tells the browser to keep the event object so it can be used later
+    // This is important because we need to access the value of the input element
     event.persist();
+    
+//setValues((values) => ({...values, address: event.target.value})),
+//updates the values object with the new value of the address input.
+//The setValues function takes a function as its argument.
+//This function takes the current values object as its input and returns
+//a new values object with the updated value of the address input.
     setValues((values) => ({
       ...values,
       address: event.target.value,
@@ -28,9 +50,9 @@ function AddressForm(props: { handler: (address: string) => void }) {
           id="public-key"
           type="text"
           placeholder="Public Address, e.g. 7C4jsPZpht42Tw6MjXWF56Q5RQUocjBBmciEjDa8HRtp"
-          name="firstName"
           value={values.address}
           onChange={handleAddressInputChange}
+          className='text-slate-800'
         />
         <br />
         <button type="submit">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,29 @@
 
 import type { NextPage } from 'next'
 import { useState } from 'react'
-import Head from 'next/head'
 import AddressForm from './AddressForm'
 import * as Web3 from '@solana/web3.js'
 
- const Home: NextPage = () => {
+//NextPage uses server side rendering for the intial render and has security features
+//defined Home as const to make it more secure
+const Home: NextPage = () => {
+
+  // useState is a React Hook that lets you add a state variable to your component.
+  // const [state, setState] = useState(initialState);
   const [balance, setBalance] = useState(0)
   const [address, setAddress] = useState('')
 
+  // addressSubmittedHandler takes an address and sets the state address.  
   const addressSubmittedHandler = (address: string) => {
     setAddress(address)
+
+    // sets a new public key based on the address
     const key = new Web3.PublicKey(address)
+
+    //creates a new connection to solana network
     const connection = new Web3.Connection(Web3.clusterApiUrl('devnet'))
+    // gets the balance from the connection using the public key then sets the state balance
+    // the state balance is set in SOL by dividing by lamports per sol.
     connection.getBalance(key).then(balance => {
       setBalance(balance / Web3.LAMPORTS_PER_SOL)
     })
@@ -21,14 +32,16 @@ import * as Web3 from '@solana/web3.js'
 
   return (
     <div>
-      <header >
-        <p>
-          Start Your Solana Journey
-        </p>
+      <main>
+        <div className='m-5 p-5 bg-slate-600 w-1/2'>
+        {/* the AddressForm handler is set to the addressSubmittedHandler above */}
         <AddressForm handler={addressSubmittedHandler} />
+
+        {/* displaying the state variales */}
         <p>{`Address: ${address}`}</p>
         <p>{`Balance: ${balance} SOL`}</p>
-      </header>
+        </div>
+        </main>
     </div>
   )
 }
